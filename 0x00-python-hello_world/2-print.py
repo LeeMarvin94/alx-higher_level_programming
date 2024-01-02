b1a515c6a18b6f6fe7bf37de58977de2e8c705cf
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2024-01-02 03:31:20 -0800

    Add done tasks

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# python script that prints a spêcified sentence
+
+print("\"Programming is like building a multilingual puzzle")
---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Prints out a number on the screen
+number = 3.14159
+print(f"Float: {number:.2f}")
