71d01bf86df6d58d5551b8c3eeec20eceb84d0c7
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2024-01-08 03:28:47 -0800

    Adding task 3

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [1, 4, 9, 3, 25]
-
-# Here I call the function
+my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
